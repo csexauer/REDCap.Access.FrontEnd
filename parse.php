48843a94c3e750b5c6ce216deaf4e41fc3c08893
Craig Sexauer <csexauer@uchicago.edu>
2012-04-16 10:17:15 -0500

    Some updates to parse

--- parse.php
+++ parse.php
@@ -1,3 +1,23 @@
+<?php
+	/* BASIC PROGRAM FLOW
+	 * 	REQUEST TO DELETE? 
+	 * 	yes										     no
+	 *  clear session, set supress					 do nothing
+	 * 	v							<---------------|
+	 *  MAIN FORM VALUES SENT?
+	 * 	yes											 no
+	 * 	FULL SCRIPT?							     print form
+	 *  yes						no			         v
+	 * 	DIRECT UPLOAD?			print func		     v
+	 * 	yes						no					 v
+	 * 	ADV SETTINGS SET		print script		 v
+	 * 	yes		no				v  					 v
+	 *  UPLOAD	ERROR			v					 v
+	 *  
+	 */
+	session_start();
+?>
+
 <!--WRITTEN BY CRAIG SEXAUER-->
 <!--FOR THE UNIVERSITY OF CHICAGO BIOLOGICAL SCIENCES DIVISION-->
 <!--APRIL 2012-->
@@ -6,7 +26,14 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Access->REDCap Transfer (beta)</title>
+  <?php
+  if(!isset($_REQUEST['redcap']))
+	 echo '<title>Access->REDCap Transfer</title>';
+  else if(isset($_REQUEST['fullprgm']) and $_REQUEST['fullprgm'])
+  	echo '<title>Import Script</title>';
+  else 
+  	echo '<title>Import Function</title>';
+  ?>
   <meta name="description" content="Access to REDCap parser">
   <meta name="author" content="Craig Sexauer">
   <meta name="viewport" content="width=device-width,initial-scale=1">
@@ -14,6 +41,9 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" type="text/javascript"></script>
 </script>
 <style type="text/css">
+	body{
+		background-color: F0F0F0;
+	}
   	p{
   		margin-bottom: 1.5em;
   	}
@@ -25,24 +55,24 @@
   		margin-top: 5px;
   		margin-left:0;
   		display: block;
-  		margin-bottom: 25px;
+  		margin-bottom: 15px;
   		width: 300px;
-  		 border-radius: 3px;
-     -moz-border-radius: 3px;
-     border: 1px solid black;
+  		border-radius: 3px;
+    	-moz-border-radius: 3px;
+ 	    border: 1px solid black;
   	}
   	textarea{
   		resize: none;
   		margin-top: 10px;
   		width: 800px;
   		display: block;
- 	   border-radius: 3px;
-     -moz-border-radius: 3px;
+ 	    border-radius: 3px;
+     	-moz-border-radius: 3px;
   	}
   	#container{
   		background-color: #F8F8F8;
-  		margin-top: 75px;
-  		margin-bottom: 100px;
+  		margin-top: 50px;
+  		margin-bottom: 50px;
   		width: 800px;
   		margin-left: auto;
   		margin-right: auto;
@@ -51,8 +81,8 @@
   		padding-top: 25px;
   		padding-bottom: 50px;
   		border: 2px dotted grey;
-  		 border-radius: 6px;
-    	-moz-border-radius: 6px;
+  		 border-radius: 10px;
+    	-moz-border-radius: 10px;
   	}
   	input{
   		display: block;
@@ -62,7 +92,11 @@
   	}
   	#usage{
   		display: none;
-  		margin-bottom: 15px;
+  		margin-bottom: 35px;
+  	}
+  	#tools{
+  		display: none;
+  		margin-bottom: 35px;
   	}
   	h1{
   		margin-bottom: 0px;
@@ -98,7 +132,6 @@
 <body>
 <div id="container">
 <?php
-	session_start();
 	$errcond = 0;
 	$supress = 0;
 	/* Clears session memory.
@@ -123,37 +156,52 @@
 					echo '<input class="smt" type="text" name="fname" value="'.$_SESSION['refname'].'"/>';
 				else
 					echo '<input class="smt" type="text" name="fname"/>';
-				echo '</label>
-					<label>Table Name:';
+				echo '</label><label>Table Name:';
 				if(isset($_SESSION['retname']))
 					echo '<input class="smt" type="text" name="tname" value="'.$_SESSION['retname'].'"/>';
 				else
 					echo '<input class="smt" type="text" name="tname"/>';
-				echo '</label></br>
-					<label>Redcap Names:';
+				echo '</label></br><label>Redcap Names:';
 				if(isset($_SESSION['rerdc']))
 					echo '<textarea class="textfield" rows=10 name="redcap"/>'.$_SESSION['rerdc'].'</textarea>';
 				else
 					echo '<textarea class="textfield" rows=10 name="redcap"/></textarea>';
-				echo '</label></br>
-					<label>Access';
+				echo '</label></br><label>Access';
 				if(isset($_SESSION['reacc']))
 					echo '<textarea class="textfield" rows=10 name="access"/>'.$_SESSION['reacc'].'</textarea>';
 				else
 					echo '<textarea class="textfield" rows=10 name="access"/></textarea>';
 				echo '</label></br>';
-				if(isset($_SESSION['fullprgm'])){
-					if($_SESSION['fullprgm'])
-						echo '<input class="radiobut" type="radio" name="fullprgm" value="1" checked/> Write a program</br>
-						<input class="radiobut" type="radio" name="fullprgm" value="0" /> Write a function';
+				if(isset($_SESSION['refullprgm'])){
+					if($_SESSION['refullprgm'])
+						echo '<input class="radiobut" type="radio" name="fullprgm" value="1" checked/> Write full php script</br>
+						<input class="radiobut" type="radio" name="fullprgm" value="0" /> Write one function';
 					else
-						echo '<input class="radiobut" type="radio" name="fullprgm" value="1" /> Write a program</br>
-						<input class="radiobut" type="radio" name="fullprgm" value="0" checked/> Write a function';
+						echo '<input class="radiobut" type="radio" name="fullprgm" value="1" /> Write full php script</br>
+						<input class="radiobut" type="radio" name="fullprgm" value="0" checked/> Write one function';
 				}
 				else
-					echo '<input class="radiobut" type="radio" name="fullprgm" value="1" /> Write a program</br>
-						<input class="radiobut" type="radio" name="fullprgm" value="0" checked/> Write a function';
-					echo '<input class="submitbutton" type="submit" value="PARSE!" /> </form>';
+					echo '<input class="radiobut" type="radio" name="fullprgm" value="1" /> Write full php script</br>
+						<input class="radiobut" type="radio" name="fullprgm" value="0" checked/> Write one function';
+				echo '</br></br><label>Advanced options for script construction.</br>
+						Leaving these blank is allowable. You can set them in the script\'s header.</br></br>
+						Access File:';
+				if(isset($_SESSION['relocal_db']))
+					echo '<input class="smt" type="text" value='.$_SESSION['relocal_db'].' name="local_db"/>';
+				else 
+					echo '<input class="smt" type="text" name="local_db"/>';
+				echo '</label><label>Your API Token:';
+				if(isset($_SESSION['reapi_tok']))
+					echo '<input class="smt" type="text" value='.$_SESSION['reapi_tok'].' name="api_tok"/>';
+				else 
+					echo '<input class="smt" type="text" name="api_tok"/>';
+				echo '</label><label>The URL of your REDCap Server:';
+				if(isset($_SESSION['rehosturl']))
+					echo '<input class="smt" type="text" value='.$_SESSION['rehosturl'] .' name="hosturl"/>';
+				else
+					echo '<input class="smt" type="text" name="hosturl"/>';
+				echo '</label><input class="radiobut" type="checkbox" name="upload" value="1"/> Upload from this script<br/>';
+				echo '<input class="submitbutton" type="submit" value="GO!" /></form>';
 				}
 	/* Handling submissions:
 	 * if no supressors are on, the script either
@@ -190,7 +238,6 @@
 			unset($_SESSION['refname']);
 		}
 		else{$_SESSION['refname'] = $form;}
-		
 		if(strlen($table) == 0){
 			if(!$errcond){	echo '<h1>Errors:</h1>'; }
 			echo '<p>Table name not specified</p>';
@@ -198,22 +245,31 @@
 			unset($_SESSION['retname']);
 		}
 		else{$_SESSION['retname'] = $table;}
-		
-		if(isset($_REQUEST['fullprgm'])){
-			$_SESSION['fullprgm'] = $_REQUEST['fullprgm'];
-		}
-		else { $_SESSION['fullprgm'] = 0;}
-		if(!isset($_REQUEST['fullprgm']) || !$_REQUEST['fullprgm']){
+		if(isset($_REQUEST['fullprgm']))
+			$_SESSION['refullprgm'] = $_REQUEST['fullprgm'];
+		else { $_SESSION['refullprgm'] = 0;}
+		if(!isset($_REQUEST['fullprgm']) || !$_REQUEST['fullprgm'])
 			$_SESSION['fullprgm'] = 0;
-		}
-		
+		if(strlen($_REQUEST['local_db']) > 0)
+			$_SESSION['relocal_db'] = $_REQUEST['local_db'];
+		else
+			unset($_SESSION['relocal_db']);
+		if(strlen($_REQUEST['api_tok']) > 0)
+			$_SESSION['reapi_tok'] = $_REQUEST['api_tok'];
+		else
+			unset($_SESSION['relocal_db']);
+		if(strlen($_REQUEST['hosturl']) > 0)
+			$_SESSION['rehosturl'] = $_REQUEST['hosturl'];
+		else
+			unset($_SESSION['rehosturl']);
+			
 		/* This is where the magic happens */
 		
 		$redcap = makerc($redcap);
 		$access = makeac($access);
 		$factors = array($redcap, $access, $form, $table);
 			if(!$errcond and !errorcheck($factors)){
-				if($_SESSION['fullprgm']){
+				if($_SESSION['refullprgm']){
 					if(isset($_SESSION['factors'])){
 						echo '<a href="parse.php">Back</a></br>'; 
 						unset($_SESSION['retname']);
@@ -301,7 +357,7 @@ function print_function($factors){
 	$access = $factors[1];
 	$form = $factors[2];
 	$table = $factors[3];
-	echo '<p>function insert_' . $form . '_value(){';
+	echo '<p>function insert_' . $form . '_values(){';
 	echo '<p>$REDCapVariables=array(';
 	for($i = 0;$i <sizeof($redcap); $i++){
 		echo '"' . $redcap[$i] . '"';	
@@ -344,7 +400,7 @@ function errorcheck($factors){
 	$errors = 0;
 	if(sizeof($factors[0]) != sizeof($factors[1])){
 		echo '<h1>Errors:</h1>'; 
-		echo '<p>The number of variables for redcap and access do not match</br>'.
+		echo '<p>The number of variables for redcap and access do not match.</br>'.
 		'REDCap: ' .sizeof($factors[0]) . '</br>Access: ' . sizeof($factors[1]) . '</p>';
 		echo '<input id="usagebutton" type="button" value="Show Details" onclick="$(\'#errordump\').toggle()" />';
 		echo '<div id="errordump"><div id="col"><p>Recap '.
@@ -354,10 +410,10 @@ function errorcheck($factors){
 			 '</p></div></div>';
 			$errors++;
 	}
-	else if($_SESSION['fullprgm'] and isset($_SESSION['factors']) and
+	else if($_SESSION['refullprgm'] and isset($_SESSION['factors']) and
 		$factors == $_SESSION['factors'][sizeof($_SESSION['factors'])-1]){
 		echo '<h1>Errors:</h1>';
-		echo '<p>It looks like you may have done a repeat submission of form '.$factors[2].'.</p>';
+		echo '<p>It looks like you attempted a repeat submission of form '.$factors[2].'.</p>';
 		$errors++;
 	}
 	else {
@@ -383,7 +439,7 @@ function errorcheck($factors){
 }
 
 function print_usage(){
-	echo '<h1>Parser</h1>
+	echo '<h1>Upload Helper</h1>
 			<input id="usagebutton" type="button" value="Usage" onclick="$(\'#usage\').toggle()" />
 			<div id="usage">
 				<blockquote>
@@ -391,13 +447,18 @@ function print_usage(){
 					i.e. 5medications if medications has 5 checkboxes</br>
 					This should be safe, because REDCap does not allow number-initiated names.</br>
 					Checkboxes fill from 0.</p>
-					<p>To make an entire program, keep the program button on and keep inputting forms.</br>
+					<p>To make an entire program, keep the script button on and keep inputting forms.</br>
 					Forms with errors will not be appended. Use the function maker for easier individual \'debugging\'.</br>
-					Use the clear all button to delete the stored program as well as the remembered form names.<p>
+					Use the clear all button to delete the stored script as well as the remembered form names.<p>
 					<p>Orders by the first element - you\'ll want your primary key here.</p>
 					<p>Tokenization occurs at carriage returns for REDCap, so you can copy it straight from data dictonary.</br>
 					Tokenization occurs at carriage returns and tabs for Access, So you can copy paste it from the table view.<\br>
 					BE NICE. Don\'t leave interstital newlines. Ends should be fine.</p>
+					<p>For the advanced options - your Access MDB file should be in the same directory as the php file, or<\br>
+					you can provide the full filepath at your own risk.</br>
+					If you have qualms about putting the token/filepath/url on the form, you can edit the variables<\br>
+					$accessfile, $api_tok, and $redcap_url in the header for the printed php script.<\br>
+					For obvious reasons, you will need those to do a direct upload.</br>
 					<p>Features I might attempt - autofill of column names from Access</p>
 		 		 </blockquote>
 		 	</div>
@@ -405,6 +466,10 @@ function print_usage(){
 				<input type="hidden" name="delete" value="1">
 				<input id="usagebutton" type="submit" value="Clear All" />
 			</form>';	
+		/*	<input id="usagebutton" type="button" value="Advanced Tools" onclick="$(\'#tools\').toggle()" />
+			<form id="tools" action="parse.php" method="POST">
+				<input type*/
+			
 }
 
 ?>
